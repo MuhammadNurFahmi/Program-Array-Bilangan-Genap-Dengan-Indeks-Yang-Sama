e81439de98e8a317e3adb17b303c0e4366605fb6
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-30 10:04:59 +0700

    Add files via upload

---
+++ array bilangan genap dengan indeks yang sama dari array a ke array b.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int c[5] = {5,10,6,0,4};
+	int d[5];
+	int i=0, j=0;
+	
+	printf("Nilai Array Yang Akan Ditampilkan \n ");
+	for (i=4; i>=0; i--)
+	{
+		d[i]=c[j];
+		printf("  %d  ", c[j]);
+		j++;
+	}
+	printf("\n");
+	printf("Nilai Array Yang Disalin Berupa Bilangan Genap Dengan Indeks Yang Sama\n");
+	for (i=4; i>=0; i--)
+	if( d[i]%2==0 && d[i] > 0)
+	{
+		printf("      %d ",d[i]);
+	}
+	
+	return 0;
+}
